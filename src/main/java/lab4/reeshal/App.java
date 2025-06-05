021581537902ecd0cca84d3930029823ce0f1865
Shraig <25febshradha.kamath@gmail.com>
2025-06-05 09:42:53 +0530

    first commit

---
+++ src/main/java/lab4/reeshal/App.java
@@ -0,0 +1,13 @@
+package lab4.reeshal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello guys" );
+    }
+}
